644845198bd42651ae1c58b1a0886ef8601961ed
wanshao <344277934@qq.com>
2022-09-24 23:09:13 +0800

    add answers

--- exercises/vecs/vecs2.rs
+++ exercises/vecs/vecs2.rs
@@ -11,7 +11,7 @@ fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
     for i in v.iter_mut() {
         // TODO: Fill this up so that each element in the Vec `v` is
         // multiplied by 2.
-        //这里可以解引用后直接修改值
+        //这里可以解引用后直接修改值.
         *i *=2;
     }
 
